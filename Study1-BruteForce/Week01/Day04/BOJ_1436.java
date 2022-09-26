68e121bce9df1234ee55a5f4cf2ef5a632830f69
ymiru0324 <ymiru0324@naver.com>
2022-09-26 22:23:34 +0900

    feat: 2022.09.26 문제풀이

---
+++ Study1-BruteForce/Week01/Day04/BOJ_1436.java
@@ -0,0 +1,22 @@
+package boj.study1_bruteforce.week01.day04;
+
+import java.util.Scanner;
+
+public class BOJ_1436 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		int jongmal = 666;
+		int cnt = 1;
+		
+		while(cnt != num) {
+			jongmal++;
+			if(String.valueOf(jongmal).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(jongmal);
+	}
+}
