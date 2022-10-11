2d473dc8c4acc66aa7ddfc4a602c1e9b7c71e73d
ymiru0324 <ymiru0324@naver.com>
2022-10-11 21:55:26 +0900

    feat: 2022.10.11 문제풀이

--- Study3-Solved_Class1/Week4/Day17/BOJ_2908.java
+++ Study3-Solved_Class1/Week4/Day17/BOJ_2908.java
@@ -1,5 +1,18 @@
 package boj.study3_solved_class1.week4.day17;
 
-public class BOJ_2908 {
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class BOJ_2908 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		int num1 = Integer.parseInt(new StringBuffer(st.nextToken()).reverse().toString());
+		int num2 = Integer.parseInt(new StringBuffer(st.nextToken()).reverse().toString());
+		
+		System.out.println(Math.max(num1, num2));
+	}
 }
