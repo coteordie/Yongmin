a1f1ce7dd7dc79632cefb7895a1944f33e987536
ymiru0324 <ymiru0324@naver.com>
2022-10-11 21:50:12 +0900

    fix: 2022.10.11 문제수정

--- Study3-Solved_Class1/Week4/Day16/BOJ_10818.java
+++ Study3-Solved_Class1/Week4/Day16/BOJ_10818.java
@@ -1,5 +1,7 @@
 package boj.study3_solved_class1.week4.day16;
 
+import java.util.Scanner;
+
 public class BOJ_10818 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
