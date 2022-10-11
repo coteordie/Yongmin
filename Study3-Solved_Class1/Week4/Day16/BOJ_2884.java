2b95d8ff3efb42b239e20d87a7c824d46fd584c8
ymiru0324 <ymiru0324@naver.com>
2022-10-11 21:50:33 +0900

    feat: 2022.10.11 문제풀이

--- Study3-Solved_Class1/Week4/Day16/BOJ_2884.java
+++ Study3-Solved_Class1/Week4/Day16/BOJ_2884.java
@@ -3,5 +3,21 @@ package boj.study3_solved_class1.week4.day16;
 import java.util.Scanner;
 
 public class BOJ_2884 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		
+		if(minute < 45) {
+			hour--;
+			minute = 60 - (45 - minute);
+			if(hour < 0) {
+				hour = 23;
+			}
+			System.out.println(hour + " " + minute);
+		} else {
+			System.out.println(hour + " " + (minute - 45));			
+		}
+	}
 }
