e31a2b4b66020013c1215a94d088262fb73cb10e
ymiru0324 <ymiru0324@naver.com>
2022-10-23 16:01:28 +0900

    feat: 2022.10.23 문제풀이

--- Study3-Solved_Class1/Week5/Day21/BOJ_2475.java
+++ Study3-Solved_Class1/Week5/Day21/BOJ_2475.java
@@ -1,5 +1,20 @@
 package boj.study3_solved_class1.week5.day21;
 
-public class BOJ_2475 {
+import java.util.Scanner;
 
+public class BOJ_2475 {
+	public static void main (String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int arr[] = new int[5];
+	    int sum = 0;
+	    
+	    for(int i = 0; i < arr.length; i++) {
+	        arr[i] = sc.nextInt();
+	    }
+	    
+	    for(int i : arr) {
+	        sum += i * i;
+	    }
+		System.out.println(sum % 10);
+	}
 }
