d389739bd6948c2236306a2e1935e6231b8a9074
Yongmini <ymiru0324@naver.com>
2022-10-23 16:02:29 +0900

    fix: 2022.10.23 문제수정

--- Study3-Solved_Class1/Week5/Day22/BOJ_10172.java
+++ Study3-Solved_Class1/Week5/Day22/BOJ_10172.java
@@ -3,9 +3,9 @@ package boj.study3_solved_class1.week5.day22;
 public class BOJ_10172 {
 	public static void main (String[] args) {
 	    System.out.println("|\\_/|\n"
-        				+ "|q p|   /}\n"
-        				+ "( 0 )\"\"\"\\\n"
-        				+ "|\"^\"`    |\n"
-        				+ "||_/=\\\\__|");
+        			+ "|q p|   /}\n"
+        			+ "( 0 )\"\"\"\\\n"
+        			+ "|\"^\"`    |\n"
+        			+ "||_/=\\\\__|");
 	}
 }
